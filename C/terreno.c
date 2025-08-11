7c45151abb35fd30cfd37e5f5190797fb1fe597d
Max <maxthomaz2006@gmail.com>
2025-08-11 16:10:05 -0300

    commitando atualizacoes do programa de terreno

--- C/terreno.c
+++ C/terreno.c
@@ -9,7 +9,7 @@ void limpar_entrada(){
 int main()
 {
     double larg;
-    double alt;
+    double compr;
     double val_m;
     double valor_t;
     double area;
@@ -17,19 +17,19 @@ int main()
     scanf("%lf", &larg);
     limpar_entrada();
 
-    printf("digite o valor da altura do terreno:\n");
-    scanf("%lf", &alt);
+    printf("digite o valor da comprimento do terreno:\n");
+    scanf("%lf", &compr);
     limpar_entrada();
 
     printf("digite o valor em reais por metro quadrado:\n");
     scanf("%lf", &val_m);
     limpar_entrada();
     
-    area = (larg*alt);
+    area = (larg*compr);
     valor_t = (val_m*area);
 
 
-    printf("Area do terreno: %.2lf valor total: %.2lf", area, valor_t);
+    printf("Area do terreno: %.2lf valor total: R$%.2lf", area, valor_t);
     
 
 
