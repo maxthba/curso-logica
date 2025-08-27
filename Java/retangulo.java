714a1cc296431b50cb1afca69c20a557de1fe771
Max <maxthomaz2006@gmail.com>
2025-08-27 16:19:41 -0300

    commitando exercicio de retangulo

---
+++ Java/retangulo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class retangulo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        double base, altura, area, perimetro, diagonal;
+
+        System.out.print("digite a base do retangulo: ");
+        base = sc.nextDouble();
+
+        System.out.print("digite a altura do retangulo: ");
+        altura = sc.nextDouble();
+
+        area = base*altura;
+        perimetro = 2*base * 2*altura;
+        diagonal = Math.sqrt(base*base + altura*altura);
+
+        System.out.println("base: "+base+"\naltura: "+altura+"\narea: "+String.format("%.4f", area)+"\nperimetro: "+String.format("%.4f", perimetro)+" \ndiagonal: "+String.format("%.4f", diagonal)+"" );
+
+    }
+}
