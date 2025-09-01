8bb5d8f016fb1ec918d3a0aa86afb1831393686b
Max <maxthomaz2006@gmail.com>
2025-09-01 16:17:40 -0300

    commitando primeiros exercicios em C#

---
+++ C#/SaidaDeDados/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace SaidaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bom Dia");
+            Console.WriteLine("Boa Noite");
+            Console.Write("Bom Dia");
+            Console.WriteLine("Boa Noite");
+
+            int x, y;
+            x = 10;
+            y = 20;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            double l;
+            l = 2.3456;
+            Console.WriteLine(l.ToString("F2", CultureInfo.InvariantCulture));
+
+            int idade;
+            double salario;
+            string nome;
+            char sexo;
+            idade = 32;
+            salario = 4560.9;
+            nome = "Maria Silva";
+            sexo = 'F';
+
+            Console.WriteLine("A funcionaria " + nome + ", sexo " + sexo + ", ganha "
+            + salario.ToString("F2", CultureInfo.InvariantCulture) +" e tem "+ idade +" anos");
+
+        }
+    }
+}
---
+++ C#/TiposVariaveis/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TipoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            double salario, altura;
+            char genero;
+            string nome;
+
+            idade = 32;
+            salario = 4000.9;
+            altura = 1.79;
+            genero = 'F';
+            nome = "Maria Silva";
+
+            Console.WriteLine(idade);
+            Console.WriteLine(salario);
+            Console.WriteLine(altura);
+            Console.WriteLine(genero);
+            Console.WriteLine(nome);
+        }
+    }
+}
