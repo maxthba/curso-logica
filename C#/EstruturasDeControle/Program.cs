2e7b550fb9f01aafa9267cca18df6f5a78ff59a3
Max <maxthomaz2006@gmail.com>
2025-09-02 16:39:27 -0300

    commitando exercicio de estruturas de controles (apenas a parte com if)

---
+++ C#/EstruturasDeControle/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+namespace EstruturaDeControle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hora;
+
+            System.Console.Write("digite uma hora: ");
+            hora = int.Parse(Console.ReadLine());
+
+            if (hora < 12)
+            {
+                Console.WriteLine("Bom dia!!");
+            }
+            else if (12 < hora && hora<18)
+            {
+                Console.WriteLine("Boa tarde!!");
+            }
+            else {
+                Console.WriteLine("Boa noite!!");
+            }
+        }
+    }
+}
