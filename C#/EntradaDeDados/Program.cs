c35057ded156b43430d7f68141621c08d349bf8b
Max <maxthomaz2006@gmail.com>
2025-09-02 16:28:28 -0300

    commitando exercicio de Entrada de dados

---
+++ C#/EntradaDeDados/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+
+namespace EntradaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario1, salario2;
+            string nome1, nome2;
+            int idade;
+            char genero;
+
+            Console.Write("Digite o nome da primeira pessoa: ");
+            nome1 = Console.ReadLine();
+            Console.Write("Digite o salario da primeira pessoa: ");
+            salario1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o nome da segunda pessoa: ");
+            nome2 = Console.ReadLine();
+            Console.Write("Digite o salario da segunda pessoa: ");
+            salario2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o sexo: ");
+            genero = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nome1 = " + nome1);
+            Console.WriteLine("Salario1 = " + salario1);
+            Console.WriteLine("Nome2 = " + nome2);
+            Console.WriteLine("Salario2 = " + salario2);
+            Console.WriteLine("Idade = " + idade);
+            Console.WriteLine("Genero = " + genero);
+
+        }
+    }
+}
